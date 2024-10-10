46afe68d834132eee0044466568ad02780b76e11
Danial Sharifrazi <danial.sharifrazi@gmail.com>
2024-10-10 18:15:04 +1100

    code upload

---
+++ Regression_by_TransferLearning.py
@@ -0,0 +1,163 @@
+import numpy as np
+import pandas as pd
+import cv2
+import os
+import glob
+from sklearn.model_selection import train_test_split,KFold
+import datetime
+from sklearn.metrics import (mean_squared_error,mean_absolute_error,mean_absolute_percentage_error)
+
+
+
+def print_results(lst_mse,lst_rmse,lst_mae,lst_mape,lst_times,model_name):    
+    import numpy as np
+    results_path=f'./results_{model_name}.txt'
+    f1=open(results_path,'a')
+
+    f1.write('\nAverage MSE: '+str(np.mean(lst_mse)))
+    f1.write('\nAverage RMSE: '+str(np.mean(lst_rmse)))
+    f1.write('\nAverage MAE: '+str(np.mean(lst_mae)))
+    f1.write('\nAverage MAPE: '+str(np.mean(lst_mape)))
+    f1.write('\nAverage Training Time: '+str(np.mean(lst_times)))
+
+    f1.write('\n\n\nMetrics for all Folds: \n')
+    for i in range(len(lst_mse)):
+        f1.write('\n MSE: '+str(lst_mse[i]))
+        f1.write('\n RMSE: '+str(lst_rmse[i]))
+        f1.write('\n MAE: '+str(lst_mae[i]))
+        f1.write('\n MAPE: '+str(lst_mape[i]))
+        f1.write('\nTraining Time: '+str(lst_times[i]))
+        f1.write('\n\n___________________\n')
+    f1.close()
+
+
+
+def outlier_detection(x_data,y_data):
+
+    from sklearn.ensemble import IsolationForest as outlier_detector
+    model=outlier_detector()
+    predicts=model.fit_predict(x_data,y_data)
+ 
+    x_data2=[]
+    y_data2=[]
+    for i in range(len(predicts)):
+        if predicts[i]==1:
+            x_data2.append(x_data[i])
+            y_data2.append(y_data[i])
+
+    x_data=np.array(x_data2)
+    y_data=np.array(y_data2)
+    return x_data,y_data
+
+
+def create_model():
+    from keras.models import Model
+    from keras.layers import Dense, GlobalAveragePooling2D
+    from keras.applications import ResNet101V2 as pre_trained_net
+
+    base_model = pre_trained_net(weights='imagenet', include_top=False)
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1024, activation='relu')(x)
+    model_output = Dense(1)(x)
+    model = Model(inputs=base_model.input, outputs=model_output)
+
+    for layer in base_model.layers:
+        layer.trainable = False
+    
+    model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+    model_name = 'ResNet101V2'
+    return model, model_name
+
+
+
+def read_data():
+
+    # Load images and labels
+    main_path='./Converted Images/'
+    image_addresses=glob.glob(main_path+'*.png')
+
+    images=[]
+    labels=[]
+    for address in image_addresses:
+        # Load labels (age)
+        image_name=os.path.splitext(os.path.basename(address))[0]
+        age=image_name.split('Age')[1].split('_')[0]
+        if age!='None':
+            labels.append(age)
+        
+            # Load images
+            img=cv2.imread(address)
+            img = cv2.resize(img, (224, 224))
+            img=img.astype('float64')
+            img=(img-np.min(img))/(np.max(img)-np.min(img))
+            images.append(img)
+
+    return np.array(images),np.array(labels)
+
+
+# Laod data and preprocess
+images,labels=read_data()
+labels=labels.astype('float')
+print('Data shape: ',images.shape,labels.shape)
+
+# Outlier detection
+image_shape=images.shape
+images=np.reshape(images, (images.shape[0],images.shape[1]*images.shape[2]*images.shape[3]))
+images,labels=outlier_detection(images,labels)
+images=np.reshape(images, (images.shape[0],image_shape[1],image_shape[2],image_shape[3]))
+print('Data shape after outlier detection: ',images.shape,labels.shape)
+
+
+lst_mse,lst_rmse,lst_mae,lst_mape,lst_times=[],[],[],[],[]
+fold_number=1
+kf=KFold(n_splits=5,random_state=42,shuffle=True)
+for train ,test in kf.split(images, labels): 
+
+    x_train=images[train]
+    x_test=images[test]
+    y_train=labels[train]
+    y_test=labels[test]
+
+    # Load model
+    model,model_name=create_model()
+
+
+    # Prepare the model
+    model.fit(x_train,y_train, epochs=10, batch_size=128)
+    for layer in model.layers[:-10]:
+        layer.trainable = False
+    for layer in model.layers[-10:]:
+        layer.trainable = True
+    model.compile(optimizer='adam', loss='mse',metrics=['mae'])
+
+    # Train the model and make predictions
+    start_time=datetime.datetime.now()
+    model.fit(x_train,y_train, epochs=20 ,batch_size=128)
+    end_time=datetime.datetime.now()
+    training_time=end_time-start_time
+
+    predicts=model.predict(x_test)
+    actuals=y_test
+    model.save(f'./results/Deep_models/{model_name}_fold{fold_number}.h5')
+
+    # Calculate evaluation metrics
+    lst_mse.append(mean_squared_error(actuals,predicts))
+    lst_rmse.append(mean_squared_error(actuals,predicts)**0.5)
+    lst_mae.append(mean_absolute_error(actuals,predicts))
+    lst_mape.append(mean_absolute_percentage_error(actuals,predicts))
+    lst_times.append(training_time)
+
+
+    # Save predictions (actuals and predicts)
+    predicts_path=f'./results/Deep_models/{model_name}_predicts_fold{fold_number}.xlsx'
+    actuals=pd.DataFrame(actuals)
+    predicts=pd.DataFrame(predicts)
+    merged_df=pd.concat([actuals,predicts],axis=1)
+    merged_df.to_excel(predicts_path)
+
+    fold_number+=1
+
+
+# Save results
+print_results(lst_mse,lst_rmse,lst_mae,lst_mape,lst_times,model_name)
---
+++ Save_images_and_ages.py
@@ -0,0 +1,99 @@
+import os
+import csv
+import numpy as np
+import cv2
+import pydicom
+import nibabel as nib
+import re
+
+
+def load_images_with_ids(image_folder):
+    images = []
+    image_ids = []
+    
+    # Traverse the directory and load images
+    for root, _, files in os.walk(image_folder):
+        folder_id = os.path.basename(root)  
+        for file in files:
+            if file.endswith(('.dcm', '.mnc')):
+                file_path = os.path.join(root, file)
+                if re.search(r'.*dcm$',file_path)!=None:               
+                    # Load MNC image
+                    img=pydicom.dcmread(file_path)
+                    img=img.pixel_array
+                else:
+                    # Load DCM image
+                    img=nib.load(file_path)
+                    img=img.get_fdata()
+                    img=img[:,:,img.shape[2]//2]
+                
+                img=cv2.resize(img,(120,100))
+                img=img.astype('uint8')               
+                images.append(img)
+
+                # Find image id in its name
+                parts=root.split('\\') 
+                image_id=parts[-4]
+                image_ids.append(image_id)
+    return np.array(images), image_ids
+
+
+def load_labels_from_csv(csv_file):
+    labels = {}
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            image_id = row['Subject']  
+            label = row['Age']  
+            labels[image_id] = label
+    return labels
+
+
+def allocate_labels_to_images(images, image_ids, labels_dict):
+    image_labels = []
+    
+    for image_id in image_ids:
+        if image_id in labels_dict:
+            image_labels.append(labels_dict[image_id])
+        else:
+            image_labels.append(None)  
+    
+    return np.array(image_labels)
+
+
+def main(image_folder, csv_file):
+    # Load images and their IDs
+    images, image_ids = load_images_with_ids(image_folder)
+    
+    # Load labels from the CSV
+    labels_dict = load_labels_from_csv(csv_file)
+    
+    # Allocate labels to images
+    image_labels = allocate_labels_to_images(images, image_ids, labels_dict)
+    
+    # Now you can return the images and labels as numpy arrays
+    return images, image_labels
+
+
+# Example usage:
+# image_folder='D:/A Researcher/Articles/1 Datasets/ICBM T1-Weighted/mini'
+image_folder='./ICBM T1-Weighted/ICBM'
+
+csv_file='./ICBM_T1_Labels.csv'
+
+images, image_labels = main(image_folder, csv_file)
+
+
+# Save images and labels
+saving_path='./Converted Images/'
+counter=1
+for image,label in zip(images,image_labels):
+    cv2.imwrite(saving_path+f'ImageAge{label}_id{counter}.png',image)
+    counter+=1
+
+
+print("Images array shape:", images.shape)
+print("Labels array shape:", image_labels.shape)
+
+
+
